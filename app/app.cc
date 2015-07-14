f64a74981cc800868682572d7a268392982c848a
Ben Bergen <bergen@lanl.gov>
2015-07-13 22:41:09 -0600

    Updated app to reflect cinch changes

--- app/app.cc
+++ app/app.cc
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <mylib/example_src/example.h>
 #include <ourlib/example_dir/print.h>
-#include <util/upart.h>
+#include <testlib/util/upart.h>
 
 int main(int argc, char ** argv) {
 
