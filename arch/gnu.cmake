2126efda2d34b8f53392b02d6f6b2ebbbab10f98
Ben Bergen <ben.bergen@gmail.com>
2015-10-26 11:26:50 -0600

    Removed unneeded file.

--- arch/gnu.cmake
+++
@@ -1,6 +0,0 @@
-#------------------------------------------------------------------------------#
-# Architecture configuration for GNU
-#------------------------------------------------------------------------------#
-
-set(CMAKE_CXX_COMPILER /opt/local/bin/g++)
-set(CMAKE_CC_COMPILER /opt/local/bin/gcc)
