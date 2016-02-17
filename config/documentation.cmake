d1b335a8a12ea2cd8e35749c1bc8114f8314f88f
Ben Bergen <bergen@lanl.gov>
2016-02-17 10:02:44 -0700

    Added documentation target

--- config/documentation.cmake
+++ config/documentation.cmake
@@ -3,6 +3,9 @@
 # All rights reserved.
 #~----------------------------------------------------------------------------~#
 
+cinch_add_doc(nested-ug ugconfig.py src
+    nested-ug-${${PROJECT_NAME}_VERSION}.pdf)
+
 #~---------------------------------------------------------------------------~-#
 # Formatting options for emacs and vim.
 # vim: set tabstop=4 shiftwidth=4 expandtab :
